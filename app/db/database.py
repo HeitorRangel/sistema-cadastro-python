d42f9b47b82b5a3cebb27ae5784c13a171d224c9
unknown <heitorespinosa@gmail.com>
2025-02-20 08:53:05 -0300

    Conferindo commits

--- app/db/database.py
+++ app/db/database.py
@@ -14,7 +14,7 @@ def criar_tabelas():
         
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS usuarios (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                 nome TEXT NOT NULL,
                 idade INTEGER NOT NULL,
                 cidade TEXT NOT NULL,
